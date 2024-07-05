5ed09ac1a5a4fda80c7da52345ebb2529f28aa90
Dipayan Das <80384778+dipayandas600@users.noreply.github.com>
2024-07-05 13:28:39 -0700

    update css and js files.

---
+++ js/sticky.js
@@ -0,0 +1,7 @@
+const hamburger = document.querySelector(".hamburger");
+const navberLinks = document.querySelector(".navber-links");
+
+hamburger.addEventListener("click", () => {
+    hamburger.classList.toggle("active");
+    navberLinks.classList.toggle("active");
+})
