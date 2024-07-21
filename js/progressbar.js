76dbf77e4f3a9ed35039b5aa5a2d44cfe0213c09
Dipayan Das <80384778+dipayandas600@users.noreply.github.com>
2024-07-22 03:10:46 +0530

    update js/progressbar.js

--- js/progressbar.js
+++ js/progressbar.js
@@ -1,22 +1,22 @@
-document.addEventListener('DOMContentLoaded', () => {
-    const progressBars = document.querySelectorAll('.progress');
-    const percentages = document.querySelectorAll('.percentage');
+document.addEventListener("DOMContentLoaded", () => {
+  const progressBars = document.querySelectorAll(".progress");
+  const percentages = document.querySelectorAll(".percentage");
 
-    progressBars.forEach(progress => {
-        const value = progress.getAttribute('data-skill');
-        progress.style.width = `${value}%`;
-    });
+  progressBars.forEach(progress => {
+    const value = progress.getAttribute("data-skill");
+    progress.style.width = `${value}%`;
+  });
 
-    percentages.forEach(percentage => {
-        const value = percentage.getAttribute('data-skill');
-        let start = 0;
-        const interval = setInterval(() => {
-            if (start >= value) {
-                clearInterval(interval);
-            } else {
-                start++;
-                percentage.textContent = `${start}%`;
-            }
-        }, 20);
-    });
+  percentages.forEach(percentage => {
+    const value = percentage.getAttribute("data-skill");
+    let start = 0;
+    const interval = setInterval(() => {
+      if (start >= value) {
+        clearInterval(interval);
+      } else {
+        start++;
+        percentage.textContent = `${start}%`;
+      }
+    }, 20);
+  });
 });
