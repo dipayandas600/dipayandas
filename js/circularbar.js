c8c571714774f0f6815cc07a34f9a6e090167e1f
Dipayan Das <80384778+dipayandas600@users.noreply.github.com>
2024-07-30 23:58:53 -0700

    update js/circularbar.js

--- js/circularbar.js
+++ js/circularbar.js
@@ -1,19 +1,21 @@
-document.addEventListener('DOMContentLoaded', () => {
-    const circularProgress = document.querySelector('.circular-progress');
-    const progressValue = circularProgress.querySelector('.progress-value');
-    const progress = circularProgress.getAttribute('data-progress');
+document.addEventListener("DOMContentLoaded", () => {
+  const circularProgress = document.querySelector(".circular-progress");
+  const progressValue = circularProgress.querySelector(".progress-value");
+  const progress = circularProgress.getAttribute("data-progress");
 
-    let startValue = 0;
-    const endValue = parseInt(progress);
-    const speed = 20; // Adjust for speed
+  let startValue = 0;
+  const endValue = parseInt(progress);
+  const speed = 20; // Adjust for speed
 
-    const progressInterval = setInterval(() => {
-        startValue++;
-        progressValue.textContent = `${startValue}%`;
-        circularProgress.style.background = `conic-gradient(#6a1b9a ${startValue * 3.6}deg, #e0e0e0 ${startValue * 3.6}deg)`;
+  const progressInterval = setInterval(() => {
+    startValue++;
+    progressValue.textContent = `${startValue}%`;
+    circularProgress.style.background = `conic-gradient(#6a1b9a ${
+      startValue * 3.6
+    }deg, #e0e0e0 ${startValue * 3.6}deg)`;
 
-        if (startValue >= endValue) {
-            clearInterval(progressInterval);
-        }
-    }, speed);
+    if (startValue >= endValue) {
+      clearInterval(progressInterval);
+    }
+  }, speed);
 });
