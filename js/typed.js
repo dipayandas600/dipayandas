e193dc38b57bbf3e90d9dcc0059910c7155d86a0
Dipayan Das <80384778+dipayandas600@users.noreply.github.com>
2024-09-06 03:17:01 -0700

    update html and css files. and create a new javascript file.

---
+++ js/typed.js
@@ -0,0 +1,5 @@
+var typed = new Typed('#element' , {
+    strings: ['Singer', 'Song Writer', 'Composer', 'Music Maker', 'Editor', 'You Tuber', 'Group Performer', 'Frontend Web Developer', 'Web Designer', 'UI/UX Designer',
+               'Figma Web Designer', 'Responsive Web Designer'],
+    typeSpeed: 50,
+});
